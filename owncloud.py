0681022fabbe307e8cc9db58c010e0c0b01479b1
Yunhuan Li <yunhuanli@Yunhuans-Mac-mini.local>
2016-10-09 20:33:42 +0800

    update functions

--- owncloud.py
+++ owncloud.py
@@ -1,53 +1,137 @@
 # -*- coding:utf-8 -*-
 
 import base64
+import urllib
 import urllib2
-from xml.etree import ElementTree
-
-USERNAME = 'admin'
-PASSWORD = 'Slim.Shady'
-BASE_URL = 'http://192.168.31.99/owncloud'
+import json
 
 class Utilies(object):
 
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
     def auth_get(self, url):
         request = urllib2.Request(url)
-        base64string = base64.b64encode('%s:%s' % (USERNAME, PASSWORD))
+        base64string = base64.b64encode('%s:%s' % (self.username, self.password))
         request.add_header("Authorization", "Basic %s" % base64string)
         response = urllib2.urlopen(request)
-        return response.read()
+        data = response.read()
+        return json.loads(data)['ocs']
 
-    def read_xml(self, data):
-        root = ElementTree.fromstring(data)
+    def auth_post(self, url, para_dict):
+        para_data = urllib.urlencode(para_dict)
+        request = urllib2.Request(url, para_data)
+        base64string = base64.b64encode('%s:%s' % (self.username, self.password))
+        request.add_header("Authorization", "Basic %s" % base64string)
+        response = urllib2.urlopen(request)
+        data = response.read()
+        return json.loads(data)['ocs']
 
     def check_status(self, data):
-        status_code = self.get_status(data)[1]
-        if status_code == '100':
+        if data['meta']['statuscode'] == 100:
             return True
         else:
             return False
 
     def get_status(self, data):
-        root = ElementTree.fromstring(data)
-        meta_node = root.find('meta')
-        status_code = meta_node.find('statuscode').text
-        status = meta_node.find('status').text
-        return status, status_code
+        return data['meta']['status'], data['meta']['statuscode'], data['meta']['message']
 
 class OwncloudManager(object):
 
-    utilies = Utilies()
+    base_url = ''
+    username = ''
+    password = ''
+
+
+    def __init__(self):
+        base_url = raw_input('server address: ')
+        if not base_url.startswith('http://') and not base_url.startswith('https://'):
+            base_url = 'http://' + base_url
+        username = raw_input('admin username: ')
+        password = raw_input('admin password: ')
+        self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.utilies = Utilies(username, password)
 
     def get_users(self):
-        url = '%s/ocs/v1.php/cloud/users' % BASE_URL
+        url = '{}/ocs/v1.php/cloud/users?format=json'.format(self.base_url)
         data = self.utilies.auth_get(url)
         if self.utilies.check_status(data):
-            return data
+            users = data['data']['users']
+            print '{} users registered:'.format(len(users))
+            for user in users:
+                print user
         else:
-            status, status_code = self.utilies.get_status(data)
-            return 'get user error!: {} {}'.format(status, status_code)
+            status, status_code, message = self.utilies.get_status(data)
+            print 'get user error!: {}'.format(message)
 
+    def get_user(self, userid):
+        url = '{}/ocs/v1.php/cloud/users/{}?format=json'.format(self.base_url, userid)
+        data = self.utilies.auth_get(url)
+        if self.utilies.check_status(data):
+            print data
+        else:
+            status, status_code, message = self.utilies.get_status(data)
+            print 'get user error!: {}'.format(message)
+
+    def created_user(self):
+        url = '{}/ocs/v1.php/cloud/users?format=json'.format(self.base_url)
+        userid = raw_input('userid: ')
+        password = raw_input('password: ')
+        para = {
+            'userid': userid,
+            'password': password
+        }
+        data = self.utilies.auth_post(url, para)
+        if self.utilies.check_status(data):
+            print "create user ok"
+        else:
+            status, status_code, message = self.utilies.get_status(data)
+            print 'create user error!: {}'.format(message)
+
+    def create_group(self):
+        url = '{}/ocs/v1.php/cloud/groups?format=json'.format(self.base_url)
+        groupid = raw_input('groupid: ')
+        para = {
+            'groupid': groupid
+        }
+        data = self.utilies.auth_post(url, para)
+        if self.utilies.check_status(data):
+            print "create group ok"
+        else:
+            status, status_code, message = self.utilies.get_status(data)
+            print 'create group error!: {}'.format(message)
+
+    def add_user_to_group(self):
+        userid = raw_input('userid: ')
+        url = '{}/ocs/v1.php/cloud/users/{}/groups?format=json'.format(self.base_url, userid)
+        groupid = raw_input('groupid: ')
+        para = {
+            'groupid': groupid
+        }
+        data = self.utilies.auth_post(url, para)
+        if self.utilies.check_status(data):
+            print "add user to group ok"
+        else:
+            status, status_code, message = self.utilies.get_status(data)
+            print 'add user to group error!: {}'.format(message)
 
 if __name__ == '__main__':
     manager = OwncloudManager()
-    print manager.get_users()
+    while True:
+        command = raw_input('owncloud> ').split(' ')
+        if command[0] == 'reset':
+            manager = OwncloudManager()
+        elif command[0] == 'exit':
+            print 'exit owncloud tool shell'
+            return
+        elif command[0] == 'get_users':
+            pass
+        else:
+            print 'unknown command'
+
+    manager.create_group()
